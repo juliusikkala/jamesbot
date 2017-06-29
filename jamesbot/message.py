7af076dd84d2de25a2e40b88f5e0ff65c7c20c8c
Julius Ikkala <julius.ikkala@gmail.com>
2017-06-29 23:34:53 +0300

    Added sticker recording

--- jamesbot/message.py
+++ jamesbot/message.py
@@ -1,16 +1,29 @@
 import json
 
 class Message:
-    def __init__(self, user_id, text):
+    def __init__(self, user_id, text, sticker):
         self.user_id = user_id
         self.text = text
+        self.sticker = sticker
 
     def from_telegram(tg_message):
-        return Message(tg_message.from_user.id, tg_message.text);
+        return Message(
+            tg_message.from_user.id,
+            tg_message.text,
+            tg_message.sticker.file_id
+        );
 
     def from_json(json_message):
         message_dict = json.loads(json_message)
-        return Message(message_dict["user_id"], message_dict["text"]);
+        return Message(
+            message_dict["user_id"],
+            message_dict["text"],
+            message_dict.get("sticker", None),
+        );
 
     def to_json(self):
-        return json.dumps({"user_id": self.user_id, "text": self.text});
+        return json.dumps({
+            "user_id": self.user_id,
+            "text": self.text,
+            "sticker": self.sticker
+        });
